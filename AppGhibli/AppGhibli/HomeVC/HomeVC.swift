98d4443ad2a90df9c12e172c43519415584175e3
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-02 23:58:37 -0300

    chore: remove storyboard e configura UIWindow no SceneDelegate

---
+++ AppGhibli/AppGhibli/HomeVC/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppGhibli
+//
+//  Created by Jenifer Rocha on 02/04/25.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
