e4decc8b46cf33364bcc568b8e84605b394c81f2
jenifer rocha <jeniferdutra2000@icloud.com>
2025-04-03 20:07:03 -0300

    feat: cria HomeScreen com UICollectionView e protocolo

---
+++ AppGhibli/AppGhibli/HomeVC/Extension/UIView + Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIView + Extension.swift
+//  AppGhibli
+//
+//  Created by Jenifer Rocha on 03/04/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor)])
+    }
+}
---
+++ AppGhibli/AppGhibli/HomeVC/Screen/HomeScreen.swift
@@ -0,0 +1,43 @@
+//
+//  HomeScreen.swift
+//  AppGhibli
+//
+//  Created by Jenifer Rocha on 03/04/25.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+    
+    lazy var collectionView: UICollectionView = { // Criacao da Collection
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.showsHorizontalScrollIndicator = false
+        // TO DO: REGISTER
+        cv.backgroundColor = .red
+        return cv
+    }()
+    
+    public func configProtocolsCollectionView(delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = dataSource
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
+        addSubview(collectionView)
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configConstraints() {
+        collectionView.pin(to: self)
+    }
+}
